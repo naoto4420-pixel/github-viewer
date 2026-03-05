db863830cbb049f04fad56a8bd3536cca20820b4
naoto4420-pixel <naoto4420@gmail.com>
2026-03-05 17:12:16 +0900

    ベース部分作成
    GitHubから情報を取得して表示する機能を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.tsx
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+interface GitHubUser {
+  login: string;
+  id: number;
+  html_url: string;
+  public_repos: number;
+};
+
+const App = () => {
+  const [user, setUser] = useState<GitHubUser | null>(null);
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      const response = await fetch('https://api.github.com/users/naoto4420-pixel');
+      const data = await response.json();
+
+      setUser(data);
+    };
+
+    fetchUserInfo();
+
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div style ={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>GitHub ユーザー情報</h1>
+      {/* userデータが存在するかどうかで、表示を切り替える*/}
+      {user ? (
+        <div style={{ border: '1px solid #ccc', padding: '10px', borderRadius: '8px' }}>
+          <p><strong>ユーザー名:</strong> {user.login}</p>
+          <p><strong>ID:</strong> {user.id}</p>
+          <p><strong>URL:</strong> <a href={user.html_url} target="_blank" rel="noreferrer">{user.html_url}</a></p>
+          <p><strong>公開リポジトリ数:</strong> {user.public_repos}</p>
+        </div>
+      ) : (
+        <p>読み込み中（ローディング）...</p>
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
